f7f0a1944761e4cdcf9a5304849d57a3ac9d6b3d
slevan <michaelfjohnson@gmail.com>
2016-12-14 17:23:34 -0800

    added gitignore

---
+++ Source/Section_03/Section_03GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Section_03GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION_03_API ASection_03GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
