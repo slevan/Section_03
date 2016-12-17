f626917542a2d21534554cb6f36f170aed5d54d7
slevan <michaelfjohnson@gmail.com>
2016-12-17 08:26:11 -0800

    BE29 Introducing Input Binding

--- Source/Section_03/Grabber.cpp
+++ Source/Section_03/Grabber.cpp
@@ -20,20 +20,31 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+	// UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
 	
 	/// Look for attached Physics Handle
 	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
-	if (PhysicsHandle)
-	{
+	if (PhysicsHandle) {
 		// Physics handle is found
+	} else {
+		UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"),
+			*GetOwner()->GetName());
 	}
-	else
-	{
-		UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"), *GetOwner()->GetName());
+
+	/// Look for attached Input Component (only appears at run time)
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (InputComponent) {
+		UE_LOG(LogTemp, Warning, TEXT("Input component found"));
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+	} else {
+		UE_LOG(LogTemp, Error, TEXT("%s missing input component"),
+			*GetOwner()->GetName());
 	}
 }
 
+void UGrabber::Grab() {
+	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"));
+}
 
 // Called every frame
 void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
